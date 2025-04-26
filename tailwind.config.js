e2cec6f673b90115f051e871e8dfc3de53428a34
Mostafizur Rahman <akrahman76@gmail.com>
2025-04-26 11:40:35 +0600

    Add components for portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
