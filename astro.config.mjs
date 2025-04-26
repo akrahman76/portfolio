bc684d70945af1fc9b69102771c4695f415dbf4c
Mostafizur Rahman <akrahman76@gmail.com>
2025-04-26 23:04:22 +0600

    Update config and build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [tailwind()],
     base: '/portfolio/',
     outDir: 'docs', // directly output to docs folder!
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+// export default {
+//   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['Inter', 'sans-serif'],
+//       },
+//       colors: {
+//         babyblue: '#d6eaf8', // soft blue tone
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+module.exports = {
+  content: ['./src/**/*.{html,astro,js,ts}',
+    './public/**/*.html',], // Adjust this path based on where your files are located
   theme: {
     extend: {
       fontFamily: {
@@ -11,4 +26,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
