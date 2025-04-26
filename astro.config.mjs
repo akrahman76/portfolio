50dbe596dad45fd741d45bdfd20fabc3f2f46950
Mostafizur Rahman <akrahman76@gmail.com>
2025-04-26 22:32:08 +0600

    update index.html

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: "/portfolio/",
+    base: '/portfolio/',
     outDir: 'docs', // directly output to docs folder!
 });
